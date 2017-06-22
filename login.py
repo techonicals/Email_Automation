c5cb651f70847b5e3b3fcdecac3091dc49ff6010
shivang.k <shivang.koshti@afourtech.com>
2017-06-22 16:30:48 +0530

    un change

--- login.py
+++ login.py
@@ -1,13 +1,42 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from ddt import ddt, data,file_data,unpack
+import unittest
+import math
 
-driver = webdriver.Firefox()
-driver.get("http://ymail.com")
-assert "Yahoo" in driver.title
-elem = driver.find_element_by_name("username")
-elem.clear()
-elem.send_keys("abc@yahoo.com")
-elem.send_keys(Keys.RETURN)
+@ddt
+class testing(unittest.TestCase):
+    @file_data('data.json')
+    def test_data(self, data_set):
+        print data_set['start']
 
-assert "No results found." not in driver.page_source
-driver.close()
+        # start = data_set['start']
+        # print start
+
+
+
+    # @data(1, 2)
+    # def test_greater_than_zero(self, value):
+    #     self.assertGreater(value, 0)
+
+
+# @file_data('data.json')
+# def login():
+#     try:
+#
+#         driver = webdriver.Firefox()
+#         driver.get("http://ymail.com")
+#         assert "Yahoo" in driver.title
+#         elem = driver.find_element_by_name("username")
+#         elem.clear()
+#         elem.send_keys.get('data','use')
+#         elem.send_keys(Keys.RETURN)
+#     except:
+#         assert "No results found." not in driver.page_source
+#
+#     driver.close()
+
+
+
+if __name__ == '__main__':
+    unittest.main()
