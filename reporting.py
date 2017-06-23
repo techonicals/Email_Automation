99e7812f85174dbf251d391b9c01535e9f24c649
shivang.k <shivang.koshti@afourtech.com>
2017-06-23 14:01:55 +0530

    un change

---
+++ reporting.py
@@ -0,0 +1,9 @@
+import os
+
+report_location = os.path.abspath(os.path.join(os.path.dirname(__file__), "."))
+print report_location
+
+
+f = open(os.path.join('Reports', 'file.txt'), 'w')
+f.write('This is the new file.')
+f.close()
