0234d0c40020303decbcc7516e3d33cc8c0ffe00
shivang.k <shivang.koshti@afourtech.com>
2017-06-30 15:48:23 +0530

    changes

--- login.py
+++ login.py
@@ -1,7 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from ddt import ddt, data,file_data,unpack
+from selenium.webdriver.common.by import By
 import unittest
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import NoSuchElementException
 import math
 import os
@@ -19,21 +21,36 @@ class testing(unittest.TestCase):
             driver.get(mail)
             #assert data_set['value'] in driver.title
             if mail == 'http://ymail.com':
-                elem = driver.find_element_by_id("login-username")
+                username = driver.find_element_by_id("login-username")
             elif mail == 'http://gmail.com':
-                elem = driver.find_element_by_id("identifierId")
+                username = driver.find_element_by_id("identifierId")
             else:
-
-                elem = driver.find_element_by_id("login-username")
-            elem.send_keys(data_set['value'])
-            elem.send_keys(Keys.RETURN)
-            #elem2 = driver.find_element_by_id("login-signin")
-            #elem2.click()
-            elem4 = driver.find_element_by_id("username-error")
-            if elem4.is_enabled():
-                print "Username doesnot Exist"
-            else :
-                print "Username Exists"
+                username = driver.find_element_by_id("login-username")
+            username.send_keys(data_set['value'])
+            username.send_keys(Keys.RETURN)
+            driver.implicitly_wait(3)
+            driver.find_element_by_name("password").send_keys(data_set['pass'])
+            #password.send_keys(data_set['pass'])
+            signin = driver.find_element_by_id("login-signin")
+            signin.click()
+            #driver.implicitly_wait(3)
+            inbox = driver.find_element_by_xpath(".//*[@class ='listnav-outter']")
+            if inbox.is_enabled():
+                print "logged in"
+            hover_un = driver.find_element_by_xpath(".//*[@id='uhWrapper']/table/tbody/tr/td[3]/ul/li[2]/a/b [contains(@title,'Hi')]")
+            hover = ActionChains(driver).move_to_element(hover_un)
+            hover.click().perform()
+            # driver.implicitly_wait(3)
+            driver.find_element_by_xpath(".//*[(@id='yucs-signout') and contains(@href,'https://login.yahoo.com') ]").click()
+            logo = driver.find_element_by_id("uh-logo")
+            if logo.is_displayed():
+                print "logged out"
+            # unError = driver.find_element_by_id("username-error")
+            #
+            # if unError.is_enabled():
+            #     print "Username doesnot Exist"
+            # else :
+            #     print "Username Exists"
 
     def tearDown(self):
         self.driver.close()
